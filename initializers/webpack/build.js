c5f31a164d370fb241c900f7860aa952b1619a01
Dimitri Badicean <badimalex@gmail.com>
2017-05-31 08:03:16 +0200

    [hotfix] build run

--- initializers/webpack/build.js
+++ initializers/webpack/build.js
@@ -1,15 +1,11 @@
-var path = require('path');
-const nodeModulesPath = path.join(process.cwd(), 'node_modules');
+require('babel-core/register');
+var fs = require('fs.extra');
 
-require(`${nodeModulesPath}/babel-core/register`);
+var webpack = require('webpack');
 
-var fs = require(`${nodeModulesPath}/fs-extra`);
+var config = require('./webpack.config.production.js').default;
 
-var webpack = require(`${nodeModulesPath}/webpack`);
-
-var config = require('./production.js').default;
-
-webpack(config, function(_error, stats) {
+webpack(config, function (_error, stats) {
   var manifest = stats.toJson().assetsByChunkName;
   fs.writeFile('webpack-manifest.json', JSON.stringify(manifest));
 });
